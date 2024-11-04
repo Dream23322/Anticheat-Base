e960278bd348e88a60daaeab0863bcf818b7ea48
4urxra <110651012+Dream23322@users.noreply.github.com>
2024-11-04 20:50:01 +1000

    Add some stuff to example speed check

--- scripts/checks/movement/speedA.js
+++ scripts/checks/movement/speedA.js
@@ -1,7 +1,10 @@
 import config from "../../data/config";
+import { getSpeed } from "../../utils/player/playerMotion";
 
 export function speed_a(player) {
     if(!config.modules.movement.speedA.enabled) return;
 
-    
+    if(getSpeed(player) > 2) {
+        // Placeholder
+    }
 }
