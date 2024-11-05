60a89957d99ff987c0040531b563a52b81a3f993
4urxra <110651012+Dream23322@users.noreply.github.com>
2024-11-05 13:49:00 +1000

    Run two manager functions in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,15 @@ import * as Minecraft from "@minecraft/server";
 
 // Import Movement Checks
 import { speed_a } from "./checks/movement/speedA";
+import { handleCombatClick } from "./managers/anticheat/check/combat/clickData";
+import { handleData } from "./managers/player/tagData";
 
 const world = Minecraft.world;
 const system = Minecraft.system;
 
 Minecraft.system.runInterval(() => {
     for (const player of world.getPlayers()) {
+        handleData(player);
         // Run checks
         speed_a(player);
     }
@@ -15,7 +18,8 @@ Minecraft.system.runInterval(() => {
 
 world.afterEvents.entityHitEntity.subscribe(({ hitEntity: entity, damagingEntity: player}) => {
     console.warn(`[Anticheat] Entity: ${entity} was hit by Entity: ${player.name}`);
-    player.cps++;
+    handleCombatClick(player);
+
     // Run checks
     
 });
