e9eca66bd58c67bc85ba958c0b8b21b28ca26f28
4urxra <110651012+Dream23322@users.noreply.github.com>
2024-11-05 13:46:46 +1000

    Add tag manager

---
+++ scripts/managers/player/tagData.js
@@ -0,0 +1,42 @@
+import * as Minecraft from "@minecraft/server";
+
+/**
+ * @name handleData
+ * @description Handles the player's tag data
+ * @param {object} player - The player being handled by the function
+ */
+export function handleData(player) {
+    /*
+    You shouldn't really need to edit this when you first start making your anticheat, but later on you might have to.
+    Though, be warned, changing stuff in here later on can cause some older checks to not work as intended.
+    */
+
+
+    // Handle damage
+    if(player.hasTag("anticheat_damage") && Date.now() - player.lastDamageTime > 5000) player.removeTag("anticheat_damage");
+
+
+    /*
+    Handle Movement
+    */
+
+    // Handing isMoving
+    if(
+        player.hasTag("anticheat_ismoving") &&
+        (
+            player.getVelocity().x > 0 || 
+            player.getVelocity().z > 0 || 
+            player.getVelocity().y > 0
+        )
+    ) {
+        player.addTag("anticheat_ismoving");
+    } else player.removeTag("anticheat_ismoving");
+
+    // Handle ground
+    if(player.onGround) player.addTag("anticheat_ground");
+    else player.removeTag("anticheat_ground");
+
+    // Handle jump
+    if(!player.hasTag("anticheat_jump") && player.isJumping) player.addTag("anticheat_jump")
+    else if(player.hasTag("anticheat_jump") && !player.isJumping && player.onGround) player.removeTag("anticheat_jump");
+}
