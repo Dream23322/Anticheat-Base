88e91a30ae4872f193d14cdbf97818da57274b6e
4urxra <110651012+Dream23322@users.noreply.github.com>
2024-11-07 11:23:52 +1000

    Add handleCombat

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import * as Minecraft from "@minecraft/server";
 import { handleCombatClick } from "./managers/anticheat/check/combat/clickData";
 import { handleData } from "./managers/player/tagData";
 import { runTickChecks } from "./managers/anticheat/check/checkRunner";
+import { checkOnHit } from "./checks/combat/handleCombat";
 
 const world = Minecraft.world;
 const system = Minecraft.system;
@@ -20,5 +21,6 @@ world.afterEvents.entityHitEntity.subscribe(({ hitEntity: entity, damagingEntity
     handleCombatClick(player);
 
     // Run checks
+    checkOnHit(player, entity);
     
 });
