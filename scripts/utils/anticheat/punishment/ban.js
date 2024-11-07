eec23f83d5582d80de13498f7089c53ad7fb7db4
4urxra <110651012+Dream23322@users.noreply.github.com>
2024-11-07 14:30:38 +1000

    notify admins when a player is kicked for being banned for when a players ban has expired

--- scripts/utils/anticheat/punishment/ban.js
+++ scripts/utils/anticheat/punishment/ban.js
@@ -1,3 +1,4 @@
+import { notifyAdmins } from "../../world/notify";
 
 /**
  * @name checkBan
@@ -20,7 +21,7 @@ export function checkBan(player) {
         if(time) {
             if(time < Date.now()) {
                 console.log("[Anticheat] Ban expired for player: " + player.name);
-    
+                notifyAdmins(`[Anticheat] Ban expired for player: ${player.name}`);
                 // ban expired, woo
                 player.removeTag("isBanned");
                 player.getTags().forEach(t => {
@@ -35,6 +36,7 @@ export function checkBan(player) {
 
         // Kick the player with punishment information
         try {
+            console.log("[Anticheat] Player " + player.name + " was removed for being banned! Reason: " + reason + " By: " + by + " Time: " + time);
             player.runCommandAsync(`kick "${player.name}" §r\n§l§cYOU ARE BANNED!\n§mBanned By:§r ${by || "N/A"}\n§bReason:§r ${reason || "N/A"}\n§aBan Length:§r ${time || "Permanant"}`);
         } catch (error) {
             console.warn("[Anticheat] Error removing player: " + player.name);
